c916af9e28621cd55796530390d3a74c7acd9d10
Kreyno93 <michael.wagener@neuefische.de>
2024-10-15 11:00:05 +0200

    adding security group to allow ssh and http

---
+++ sg.tf
@@ -0,0 +1,33 @@
+# Security Group
+resource "aws_security_group" "webserver_sg" {
+  vpc_id = aws_vpc.aws_capstone_vpc.id
+
+  # Allow SSH inbound traffic
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow HTTP inbound traffic
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name              = "AwsCapstoneWebServerSG"
+    SecurityGroupName = "AwsCapstoneWebServerSG"
+  }
+}
