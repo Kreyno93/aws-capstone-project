b5f12fe5108e85f00f0145b6ae91efbb8954246f
Kreyno93 <michael.wagener@neuefische.de>
2024-10-15 10:45:31 +0200

    adding first public in AZ a

--- var.tf
+++ var.tf
@@ -3,3 +3,9 @@ variable "region_us_west_2" {
   description = "value for the region"
   default     = "us-west-2"
 }
+
+variable "aws_availablity_zone_a" {
+  type        = string
+  description = "value for the availability zone"
+  default     = "us-west-2a"
+}
