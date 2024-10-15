bb6b1ad619dd297876f97bd6da1ace08847b2d18
Kreyno93 <michael.wagener@neuefische.de>
2024-10-15 10:56:41 +0200

    adding association for public and private route table

--- routetable.tf
+++ routetable.tf
@@ -20,3 +20,28 @@ resource "aws_route_table" "private_route_table" {
     Name = "PrivateRouteTable"
   }
 }
+
+# Associate Public Subnet 1 with the Route Table
+resource "aws_route_table_association" "public_subnet_1_association" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+# Associate Public Subnet 2 with the Route Table
+resource "aws_route_table_association" "public_subnet_2_association" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+# Associate Private Subnet 1 with the Route Table
+resource "aws_route_table_association" "private_subnet_1_association" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+# Associate Private Subnet 2 with the Route Table
+resource "aws_route_table_association" "private_subnet_2_association" {
+  subnet_id      = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
