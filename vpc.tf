c82b31d480561ddefe6ba7995023b6bdc590f9c6
Kreyno93 <michael.wagener@neuefische.de>
2024-10-15 10:46:44 +0200

    adding second private sub in AZ a

--- vpc.tf
+++ vpc.tf
@@ -40,3 +40,13 @@ resource "aws_subnet" "private_subnet_1" {
   }
 }
 
+# Create Private Subnet 2 in the VPC
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id                  = aws_vpc.deham14.id
+  cidr_block              = "10.0.4.0/24"
+  availability_zone       = var.aws_availability_zone_b
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "PrivateSubnet2"
+  }
+}
