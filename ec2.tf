30a8415a2a467d1bc70d0ee4fd9528d63e5e2abd
Kreyno93 <michael.wagener@neuefische.de>
2024-10-15 11:04:27 +0200

    adding code to automatically grab the newest amazon linux 2 AMI

---
+++ ec2.tf
@@ -0,0 +1,10 @@
+# Get the latest Amazon Linux 2 AMI
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
