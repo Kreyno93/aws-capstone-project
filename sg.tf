55a408ab178a29f426421be28c6513e24e8e466c
Krey <michael.wagener@neuefische.de>
2025-04-30 17:33:48 +0200

    change the inline rules for egress and ingress to its own resource block

--- sg.tf
+++ sg.tf
@@ -1,48 +1,25 @@
+
 # Security Group for SSH
 resource "aws_security_group" "aws_ssh_sg" {
   name   = "AWS-Capstone-SSH-SG"
   vpc_id = aws_vpc.aws_capstone_vpc.id
-
-  # Allow SSH inbound traffic
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  description = "Allow SSH access to EC2 instances"
 }
 
+resource "aws_vpc_security_group_ingress_rule" "aws_ssh_sg_rule" {
+  security_group_id = aws_security_group.aws_ssh_sg.id
 
-# Security Group for Load Balancer
-resource "aws_security_group" "aws_capstone_lb_sg" {
-  name   = "AWS-Capstone-LoadBalancer-SG"
-  vpc_id = aws_vpc.aws_capstone_vpc.id
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 22
+  ip_protocol = "tcp"
+  to_port     = 22
+  description = "Allow SSH access to EC2 instances"
+}
 
-  # Allow HTTP inbound traffic
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Allow all outbound traffic
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "AwsCapstoneLoadBalancerSG"
-  }
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4_ssh" {
+  security_group_id = aws_security_group.aws_ssh_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
 }
 
 # Security Group for HTTP
@@ -50,47 +27,91 @@ resource "aws_security_group" "aws_webserver_sg" {
   name   = "AWS-Capstone-WebServer-SG"
   vpc_id = aws_vpc.aws_capstone_vpc.id
 
-  # Allow HTTP inbound traffic
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Allow all outbound traffic
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   tags = {
     Name              = "AwsCapstoneWebServerSG"
     SecurityGroupName = "AwsCapstoneWebServerSG"
   }
 }
 
+resource "aws_vpc_security_group_ingress_rule" "aws_webserver_sg_rule" {
+  security_group_id = aws_security_group.aws_webserver_sg.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 80
+  ip_protocol = "tcp"
+  to_port     = 80
+  description = "Allow HTTP access to EC2 instances"
+}
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4_webserver" {
+  security_group_id = aws_security_group.aws_webserver_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
+
+# Uncomment the following block if you want to allow HTTPS access as well
+# resource "aws_vpc_security_group_ingress_rule" "aws_webserver_sg_rule_https" {
+#   security_group_id = aws_security_group.aws_webserver_sg.id
+
+#   cidr_ipv4   = "0.0.0.0/0"
+#   from_port   = 443
+#   ip_protocol = "tcp"
+#   to_port     = 443
+#   description = "Allow HTTPS access to EC2 instances"
+# }
+
+# Security Group for Load Balancer
+resource "aws_security_group" "aws_capstone_lb_sg" {
+  name   = "AWS-Capstone-LoadBalancer-SG"
+  vpc_id = aws_vpc.aws_capstone_vpc.id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "aws_capstone_lb_sg_rule" {
+  security_group_id = aws_security_group.aws_capstone_lb_sg.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 80
+  ip_protocol = "tcp"
+  to_port     = 80
+  description = "Allow HTTP access to Load Balancer"
+}
+
+# Uncomment the following block if you want to allow HTTPS access as well
+# resource "aws_vpc_security_group_ingress_rule" "aws_capstone_lb_sg_rule_https" {
+#   security_group_id = aws_security_group.aws_capstone_lb_sg.id
+
+#   cidr_ipv4   = "0.0.0.0/0"
+#   from_port   = 443
+#   ip_protocol = "tcp"
+#   to_port     = 443
+#   description = "Allow HTTPS access to Load Balancer"
+# }
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4_lb" {
+  security_group_id = aws_security_group.aws_capstone_lb_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
+
+
 # Security Group for RDS
 resource "aws_security_group" "aws_rds_sg" {
   name        = "AWS-Capstone-RDS-SG"
   description = "Allow traffic to RDS"
   vpc_id      = aws_vpc.aws_capstone_vpc.id
+}
 
-  # Allow MySQL inbound traffic
-  ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_vpc_security_group_ingress_rule" "aws_rds_sg_rule" {
+  security_group_id = aws_security_group.aws_rds_sg.id
 
-  # Allow all outbound traffic
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 3306
+  ip_protocol = "tcp"
+  to_port     = 3306
+  description = "Allow MySQL access to RDS"
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4_rds" {
+  security_group_id = aws_security_group.aws_rds_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
 }
