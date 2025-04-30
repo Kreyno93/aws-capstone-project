de6feee4c0da08f3d7741fe3112f21e360cd73a8
Krey <michael.wagener@neuefische.de>
2025-04-30 18:20:58 +0200

    add comment to keypair attribute, since key is not creating automatically | fixing security groups on the test instances and change to the new ALB testing userdata

--- ec2.tf
+++ ec2.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "bastion" {
   availability_zone      = var.aws_availability_zone_a
   vpc_security_group_ids = [aws_security_group.aws_ssh_sg.id]
   subnet_id              = aws_subnet.public_subnet_1.id
-  key_name               = "vockey"
+  key_name               = "vockey" # Create new key if using different region than us-west-2
   user_data              = file("bastion-userdata.sh")
   tags = {
     Name = "AwsCapstoneBastionHost"
@@ -30,25 +30,25 @@ resource "aws_instance" "bastion" {
 # resource "aws_instance" "web" {
 #   ami                    = data.aws_ami.amazon_linux_2.id
 #   instance_type          = var.aws_instance_type_t2micro
-#   availability_zone      = var.aws_availablity_zone_a
-#   vpc_security_group_ids = [aws_security_group.aws_capstone_sg.id]
+#   availability_zone      = var.aws_availability_zone_a
+#   vpc_security_group_ids = [aws_security_group.aws_ssh_sg.id, aws_security_group.aws_webserver_sg.id]
 #   subnet_id              = aws_subnet.public_subnet_1.id
-#   key_name               = "vockey"
-#   user_data              = file("userdata.sh")
+#   key_name               = "vockey" # Create new key if using different region than us-west-2
+#   user_data              = file("loadbalancertest.sh")
 #   tags = {
 #     Name = "AwsCapstoneWebServer"
 #   }
 # }
 
-# Second Instance for Load Balancing testing -- Will be replaced by Auto Scaling Group
+# # Second Instance for Load Balancing testing -- Will be replaced by Auto Scaling Group
 # resource "aws_instance" "web2" {
 #   ami                    = data.aws_ami.amazon_linux_2.id
 #   instance_type          = var.aws_instance_type_t2micro
-#   availability_zone      = var.aws_availablity_zone_a
-#   vpc_security_group_ids = [aws_security_group.aws_capstone_sg.id]
-#   subnet_id              = aws_subnet.public_subnet_1.id
-#   key_name               = "vockey"
-#   user_data              = file("userdata.sh")
+#   availability_zone      = var.aws_availability_zone_b
+#   vpc_security_group_ids = [aws_security_group.aws_ssh_sg.id, aws_security_group.aws_webserver_sg.id]
+#   subnet_id              = aws_subnet.public_subnet_2.id
+#   key_name               = "vockey" # Create new key if using different region than us-west-2
+#   user_data              = file("loadbalancertest.sh")
 #   tags = {
 #     Name = "AwsCapstoneWebServer2"
 #   }
