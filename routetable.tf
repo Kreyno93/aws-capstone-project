7999e3343962ec2527f4023ce682df41ee441b22
Krey <michael.wagener@neuefische.de>
2025-04-30 18:15:04 +0200

    [ADD] new route table association for the second public subnet

--- routetable.tf
+++ routetable.tf
@@ -31,6 +31,12 @@ resource "aws_route_table_association" "public_subnet_1_association" {
   route_table_id = aws_route_table.public_route_table.id
 }
 
+# Associate Public Subnet 2 with the Route Table
+resource "aws_route_table_association" "public_subnet_2_association" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
 # Associate Private Subnet 1 with the Route Table
 resource "aws_route_table_association" "private_subnet_1_association" {
   subnet_id      = aws_subnet.private_subnet_1.id
